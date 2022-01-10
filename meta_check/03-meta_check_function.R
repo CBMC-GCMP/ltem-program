db818b035d773074c61ee870c9a0a5914f418861
eLeonBM <leonsol.eduardo@gmail.com>
2022-01-10 12:17:28 -0600

    Size and Quantity check, according to historical confidence intervals

--- meta_check/03-meta_check_function.R
+++ meta_check/03-meta_check_function.R
@@ -2,27 +2,18 @@ library(tidyverse)
 
  historical <- readxl:: read_xlsx("data/ltem_monitoring.xlsx")
 
-# refs_meta <- readxl:: read_xlsx("data/refs_meta_fish.xlsx")
 
 
-# refs_meta <- historical %>% 
-#   group_by(IDSpecies, Species) %>% 
+
+# refs_meta <- historical %>%
+#   group_by(IDSpecies, Species) %>%
 #   summarise(Size_min = min(Size, na.rm=T),
 #          Size_max =max(Size, na.rm=T),
 #          Quantity_max= max(Quantity, na.rm=T))
-# 
-# 
-# 
-# test <- merge(refs_meta, clean_fish[,c("IDSpecies", "Validated")], by="IDSpecies", all=T) %>% 
-#   mutate(Species= ifelse(is.na(Validated), Species, Validated)) %>% 
-#   select(-Validated)
-# 
-# refs_meta<- merge(test, clean_inv[,c("IDSpecies", "valid_name")], by="IDSpecies", all=T) %>% 
-#   mutate(Species= ifelse(is.na(valid_name), Species, valid_name)) %>% 
-#   select(-valid_name) %>% 
-#   filter(!is.na(Quantity_max))
-# 
-# writexl::write_xlsx(refs_meta, "data/auxiliar/refs_meta.xlsx")
+
+
+
+
 
 
 refs_meta <- readxl:: read_xlsx("data/auxiliar/updated_catalogo_spp.xlsx")
